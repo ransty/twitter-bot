a5c043f9b8f047ebba322d6d1a32d2d296fbfcc9
Porcaro <porkd002@f120-11.local>
2017-08-21 20:13:34 +0930

    Default twitter bot

---
+++ bot.js
@@ -0,0 +1,26 @@
+console.log('The bot is starting');
+
+var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+tweetIt();
+setInterval(tweetIt, 60*5*1000);
+
+function tweetIt() {
+	var r = Math.floor(Math.random()*100);
+	var tweet = {
+		status: 'random number ' + r
+	}
+	
+	T.post('statuses/update', tweet, tweeted);
+	
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log('Something went wrong!');
+		} else {
+			console.log('It worked!');
+		}
+	}
+}
