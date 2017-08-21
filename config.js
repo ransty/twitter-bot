22cd0ee7e5971f189b3f30fb7b85a62fc1f7ea6a
Porcaro <porkd002@f120-11.local>
2017-08-21 20:17:39 +0930

    Added proper config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	consumer_key: '',
-	consumer_secret: '',
-	access_token: '',
-	access_token_secret: ''
+	consumer_key: 'u6wao5VNqczBRKmafJtYkts2U',
+	consumer_secret: 'dT9JZobsWvzyR7l8WJ2k1h2bV2MEuuVPYuWg7bTx9QMhTTFnWF',
+	access_token: '779592676668764160-1WZCq1EslJQ2NghxmDXNjhA5DA9iNcQ',
+	access_token_secret: 'BV722L3yFtDW1F1jaXuhBIhOoA89yPj6RaaGs8Ez3Er0O'
 }
