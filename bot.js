4040324130145a3d355b5acdb916b1f4124d8371
Keano Porcaro <keano.porcaro@gmail.com>
2018-02-17 02:27:53 +1030

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
-console.log('The bot is starting');
-
 var Twit = require('twit');
 
 var config = require('./config');
 var T = new Twit(config);
+
+const https = require("http");
+const url =
+  "http://crystalmathlabs.com/tracker/api.php?type=update&player=clam_tickler";
+http.get(url);
 
 tweetIt();
 setInterval(tweetIt, 60*5*1000);
