6173bcc52aebaf515b7ebdb90a2e3cd96ce27bad
Keano Porcaro <keano.porcaro@gmail.com>
2018-07-18 21:07:20 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,19 +11,5 @@ tweetIt();
 setInterval(tweetIt, 60*5*1000);
 
 function tweetIt() {
-	var r = Math.floor(Math.random()*1000);
-	var tweet = {
-		status: 'random number ' + r + ' #randomgenerator'
-	}
-	
 	http.get(url);
-	T.post('statuses/update', tweet, tweeted);
-	
-	function tweeted(err, data, response) {
-		if (err) {
-			console.log('Something went wrong!');
-		} else {
-			console.log('It worked!');
-		}//Fiz urself heroku
-	}
 }
